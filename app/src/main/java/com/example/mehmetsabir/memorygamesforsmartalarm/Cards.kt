85be4c8a548628d36d366badfb2c157d9fe7f62b
mehmetsabir <m.sabirkahraman@gmail.com>
2018-10-25 16:19:17 +0300

    edited folders of projects

---
+++ app/src/main/java/com/example/mehmetsabir/memorygamesforsmartalarm/Cards.kt
@@ -0,0 +1,18 @@
+package com.example.mehmetsabir.memorygamesforsmartalarm
+
+import android.widget.Button
+
+class Cards(button: Button, x: Int, y: Int) {
+
+
+    var x: Int = 0
+    var y: Int = 0
+    var button: Button
+
+    init {
+        this.x = x;
+        this.y = y
+        this.button = button
+    }
+
+}
